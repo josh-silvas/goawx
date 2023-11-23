a411ff21bba05bb90fe903ad96fece94e4b624a0
Nicolas DUPEUX <nicolas@dupeux.net>
2023-11-23 21:17:24 +0100

    feat: Add job_slice_count to JobTemplate type (#22)

--- client/types.go
+++ client/types.go
@@ -375,6 +375,7 @@ type JobTemplate struct {
 	Project               int         `json:"project"`
 	Playbook              string      `json:"playbook"`
 	Forks                 int         `json:"forks"`
+	JobSliceCount         int         `json:"job_slice_count"`
 	Limit                 string      `json:"limit"`
 	Verbosity             int         `json:"verbosity"`
 	ExtraVars             string      `json:"extra_vars"`
